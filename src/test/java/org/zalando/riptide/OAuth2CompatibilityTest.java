3bfab9910b2ff8fde6c7ee0829ca3df50a25592b
Lukas Niemeier <lukas.niemeier@student.hpi.uni-potsdam.de>
2015-06-18 19:25:34 +0200

    Use actual OAuth2 error handler

--- src/test/java/org/zalando/riptide/OAuth2CompatibilityTest.java
+++ src/test/java/org/zalando/riptide/OAuth2CompatibilityTest.java
@@ -31,6 +31,8 @@ import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 import org.zalando.riptide.OAuth2CompatibilityResponseErrorHandler;
+import org.zalando.riptide.PassThroughResponseErrorHandler;
+import org.springframework.security.oauth2.client.http.OAuth2ErrorHandler;
 
 import java.io.IOException;
 import java.net.URI;
@@ -38,27 +40,26 @@ import java.net.URI;
 import static java.util.Collections.singletonList;
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withUnauthorizedRequest;
 import static org.zalando.riptide.Conditions.on;
 import static org.zalando.riptide.Selectors.status;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class OAuth2CompatibilityTest {
 
     private final URI url = URI.create("http://localhost");
     private final RestTemplate template;
-    private final MockRestServiceServer server;
 
     public OAuth2CompatibilityTest() {
-        final OAuth2CompatibilityResponseErrorHandler handler = new OAuth2CompatibilityResponseErrorHandler();
-
         final MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         converter.setObjectMapper(new ObjectMapper().findAndRegisterModules());
 
         template = new RestTemplate();
         template.setMessageConverters(singletonList(converter));
-        template.setErrorHandler(new OAuth2ResponseErrorHandler(handler));
 
-        server = MockRestServiceServer.createServer(template);
+        MockRestServiceServer server = MockRestServiceServer.createServer(template);
+        server.expect(requestTo(url)).andRespond(withUnauthorizedRequest().body(new byte[] {0x13, 0x37}));
     }
 
     @Rule
@@ -66,30 +67,29 @@ public class OAuth2CompatibilityTest {
 
     @Test
     public void dispatchesConsumedResponseAgain() throws IOException {
-        server.expect(requestTo(url)).andRespond(withSuccess());
-
+        template.setErrorHandler(new OAuth2ErrorHandler(new OAuth2CompatibilityResponseErrorHandler(), null));
         Rest rest = Rest.create(template);
-        rest.execute(GET, url).dispatch(status(), on(HttpStatus.OK).capture()).retrieveResponse()
-            .orElseThrow(() -> new AssertionError("Expected a response"));
+        
+        ClientHttpResponse response = rest.execute(GET, url)
+            .dispatch(status(), on(HttpStatus.UNAUTHORIZED).capture())
+            .retrieveResponse()
+            .orElseThrow(() -> new AssertionError("response expected"));
+        
+        assertThat(response.getBody().available(), is(2));
     }
-
-    private static class OAuth2ResponseErrorHandler implements ResponseErrorHandler {
-
-        private final ResponseErrorHandler innerHandler;
-
-        private OAuth2ResponseErrorHandler(final ResponseErrorHandler innerHandler) {
-            this.innerHandler = innerHandler;
-        }
-
-        @Override
-        public boolean hasError(final ClientHttpResponse response) throws IOException {
-            return true;
-        }
-
-        @Override
-        public void handleError(final ClientHttpResponse response) throws IOException {
-            // The actual OAuth2ResponseErrorHandler consumes the response here - we'll just pass it on
-            innerHandler.handleError(response);
-        }
+    
+    @Test
+    public void responseIsConsumedIfOtherHandlerIsUsed() throws IOException {
+        template.setErrorHandler(new OAuth2ErrorHandler(new PassThroughResponseErrorHandler(), null));
+        Rest rest = Rest.create(template);
+        
+        ClientHttpResponse response = rest.execute(GET, url)
+            .dispatch(status(), on(HttpStatus.UNAUTHORIZED).capture())
+            .retrieveResponse()
+            .orElseThrow(() -> new AssertionError("response expected"));
+        
+        // Since our mocked response is using a byte[] stream we check for the remaining bytes instead
+        // of expecting an "already closed" IOException.
+        assertThat(response.getBody().available(), is(0));
     }
 }
