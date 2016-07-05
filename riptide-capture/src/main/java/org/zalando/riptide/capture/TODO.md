8d6a05a71aaf37353b6845116ddbec2515d44c32
Willi Schönborn <willi.schoenborn@zalando.de>
2016-07-05 22:39:04 +0200

    Added capture implementation

--- riptide-capture/src/main/java/org/zalando/riptide/capture/TODO.md
+++ riptide-capture/src/main/java/org/zalando/riptide/capture/TODO.md
@@ -1,4 +1,1 @@
-- headers
-- location
-- andThen/compose
 - responseEntityOf
---
+++ riptide-capture/src/test/java/org/zalando/riptide/capture/CaptureTest.java
@@ -0,0 +1,110 @@
+package org.zalando.riptide.capture;
+
+/*
+ * ⁣​
+ * Riptide
+ * ⁣⁣
+ * Copyright (C) 2015 Zalando SE
+ * ⁣⁣
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ​⁣
+ */
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.zalando.riptide.Rest;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.NoSuchElementException;
+import java.util.concurrent.ExecutionException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import static org.zalando.riptide.Bindings.anyStatus;
+import static org.zalando.riptide.Bindings.on;
+import static org.zalando.riptide.Navigators.status;
+
+public final class CaptureTest {
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    private final URI url = URI.create("https://api.example.com/accounts/123");
+
+    private final Rest unit;
+    private final MockRestServiceServer server;
+
+    public CaptureTest() {
+        final MockSetup setup = new MockSetup();
+        this.unit = setup.getRest();
+        this.server = setup.getServer();
+    }
+
+    @Before
+    public void setUp() {
+        server.expect(requestTo(url)).andRespond(
+                withSuccess()
+                        .body(new ClassPathResource("message.json"))
+                        .contentType(APPLICATION_JSON));
+    }
+
+    @Test
+    public void shouldCapture() throws Exception {
+        final Capture<ObjectNode> capture = Capture.empty();
+
+        unit.get("/accounts/123")
+                .dispatch(status(),
+                        on(OK).call(ObjectNode.class, capture),
+                        anyStatus().call(this::fail)).get();
+
+        final ObjectNode node = capture.retrieve();
+
+        assertThat(node.get("message").asText(), is("Hello World!"));
+    }
+
+    @Test
+    public void shouldAdapt() throws IOException, ExecutionException, InterruptedException {
+        final Capture<ObjectNode> capture = Capture.empty();
+
+        final ListenableFuture<?> future = unit.get("/accounts/123")
+                .dispatch(status(),
+                        on(OK).call(ObjectNode.class, capture),
+                        anyStatus().call(this::fail));
+
+        final ObjectNode node = capture.adapt(future).get();
+
+        assertThat(node.get("message").asText(), is("Hello World!"));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void shouldFail() {
+        Capture.empty().retrieve();
+    }
+
+    private void fail(final ClientHttpResponse response) throws IOException {
+        throw new AssertionError(response.getRawStatusCode());
+    }
+
+}
