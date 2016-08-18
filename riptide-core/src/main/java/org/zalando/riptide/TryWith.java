193bbde721fa3c6a3aff8ef4949facf7187f3d40
tkrop <tronje.krop@zalando.de>
2016-08-18 10:28:47 +0200

    improved code quality

--- riptide-core/src/main/java/org/zalando/riptide/TryWith.java
+++ riptide-core/src/main/java/org/zalando/riptide/TryWith.java
@@ -1,17 +1,37 @@
 package org.zalando.riptide;
 
+/*
+ * ⁣​
+ * Riptide: Core
+ * ⁣⁣
+ * Copyright (C) 2015 - 2016 Zalando SE
+ * ⁣⁣
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ​⁣
+ */
+
 public interface TryWith {
 
     static void tryWith(final AutoCloseable closeable, final ThrowingRunnable consumer) throws Exception {
         try {
             consumer.run();
-        } catch (Exception ex) {
+        } catch (Exception e) {
             try {
                 closeable.close();
-            } catch (Exception cex) {
-                ex.addSuppressed(cex);
+            } catch (Exception ce) {
+                e.addSuppressed(ce);
             }
-            throw ex;
+            throw e;
         }
         closeable.close();
     }
@@ -20,13 +40,13 @@ public interface TryWith {
             throws Exception {
         try {
             consumer.accept(input);
-        } catch (Exception ex) {
+        } catch (Exception e) {
             try {
                 closeable.close();
-            } catch (Exception cex) {
-                ex.addSuppressed(cex);
+            } catch (Exception ce) {
+                e.addSuppressed(ce);
             }
-            throw ex;
+            throw e;
         }
         closeable.close();
     }
