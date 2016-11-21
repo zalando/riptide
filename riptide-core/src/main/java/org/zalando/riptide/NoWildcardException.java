f0b8d2027a2e6ef1e72f1dadcf35c0b4453aeea4
Willi Schönborn <willi.schoenborn@zalando.de>
2016-11-21 14:24:09 +0100

    Made NoWildcardException not use a stack trace

--- riptide-core/src/main/java/org/zalando/riptide/NoWildcardException.java
+++ riptide-core/src/main/java/org/zalando/riptide/NoWildcardException.java
@@ -2,4 +2,13 @@ package org.zalando.riptide;
 
 public final class NoWildcardException extends RuntimeException {
 
+    /**
+     * We don't care for the stack trace, and since it's expensive to calculate we just use the same instance
+     * all the time. It's exclusively used to jump around the stack.
+     */
+    @Override
+    public Throwable fillInStackTrace() {
+        return this;
+    }
+
 }
