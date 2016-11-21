403cff92794dd7085059e71d3bf0dc7d7e7944b0
Willi Schönborn <willi.schoenborn@zalando.de>
2016-11-21 15:14:09 +0100

    Corrected comment

--- riptide-core/src/main/java/org/zalando/riptide/NoWildcardException.java
+++ riptide-core/src/main/java/org/zalando/riptide/NoWildcardException.java
@@ -3,8 +3,7 @@ package org.zalando.riptide;
 public final class NoWildcardException extends RuntimeException {
 
     /**
-     * We don't care for the stack trace, and since it's expensive to calculate we just use the same instance
-     * all the time. It's exclusively used to jump around the stack.
+     * We don't care for the stack trace. It's exclusively used to jump around the stack.
      */
     @Override
     public Throwable fillInStackTrace() {
