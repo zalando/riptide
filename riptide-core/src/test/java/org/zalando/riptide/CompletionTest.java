1890b91a8366af6065fcc2ac24336076145d1458
Willi Schönborn <willi.schoenborn@zalando.de>
2016-11-29 00:07:57 +0100

    Added static join method that rethrows the cause of a completion exception
    Fixes #176

---
+++ riptide-core/src/test/java/org/zalando/riptide/CompletionTest.java
@@ -0,0 +1,36 @@
+package org.zalando.riptide;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+
+import static java.util.concurrent.CompletableFuture.completedFuture;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.Assert.assertThat;
+import static org.zalando.riptide.Completion.join;
+
+public final class CompletionTest {
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void shouldJoinSuccessfully() {
+        assertThat(join(completedFuture("test")), is("test"));
+    }
+
+    @Test
+    public void shouldPropagateCause() {
+        final CompletableFuture<Void> future = new CompletableFuture<>();
+        final IOException e = new IOException();
+        future.completeExceptionally(e);
+
+        exception.expect(sameInstance(e));
+        join(future);
+    }
+
+}
