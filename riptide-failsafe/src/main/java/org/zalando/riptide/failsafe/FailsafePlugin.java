d49b08307c70b03c53df89baf6fee73755fd885f
Willi Schönborn <willi.schoenborn@zalando.de>
2017-09-28 21:23:15 +0200

    Removed lombok dependency in FailsafePlugin

--- riptide-failsafe/src/main/java/org/zalando/riptide/failsafe/FailsafePlugin.java
+++ riptide-failsafe/src/main/java/org/zalando/riptide/failsafe/FailsafePlugin.java
@@ -1,8 +1,5 @@
 package org.zalando.riptide.failsafe;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.experimental.Wither;
 import net.jodah.failsafe.CircuitBreaker;
 import net.jodah.failsafe.Failsafe;
 import net.jodah.failsafe.RetryPolicy;
@@ -12,19 +9,29 @@ import org.zalando.riptide.RequestExecution;
 
 import java.util.concurrent.ScheduledExecutorService;
 
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public final class FailsafePlugin implements Plugin {
 
     private final ScheduledExecutorService scheduler;
-
-    @Wither
     private final RetryPolicy retryPolicy;
-
-    @Wither
     private final CircuitBreaker circuitBreaker;
 
     public FailsafePlugin(final ScheduledExecutorService scheduler) {
         this(scheduler, new RetryPolicy(), new CircuitBreaker());
+    }
+
+    private FailsafePlugin(final ScheduledExecutorService scheduler, final RetryPolicy retryPolicy,
+            final CircuitBreaker circuitBreaker) {
+        this.scheduler = scheduler;
+        this.retryPolicy = retryPolicy;
+        this.circuitBreaker = circuitBreaker;
+    }
+
+    public FailsafePlugin withRetryPolicy(final RetryPolicy retryPolicy) {
+        return new FailsafePlugin(scheduler, retryPolicy, circuitBreaker);
+    }
+
+    public FailsafePlugin withCircuitBreaker(final CircuitBreaker circuitBreaker) {
+        return new FailsafePlugin(scheduler, retryPolicy, circuitBreaker);
     }
 
     @Override
