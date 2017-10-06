4bf2299c8bb5ab77840bfb08c1663750f69430a9
Willi Schönborn <willi.schoenborn@zalando.de>
2017-10-06 19:13:11 +0200

    Implemented retry route as singleton

--- riptide-failsafe/src/main/java/org/zalando/riptide/failsafe/RetryRoute.java
+++ riptide-failsafe/src/main/java/org/zalando/riptide/failsafe/RetryRoute.java
@@ -1,5 +1,8 @@
 package org.zalando.riptide.failsafe;
 
+import org.springframework.http.client.ClientHttpResponse;
+import org.zalando.riptide.MessageReader;
+import org.zalando.riptide.NoRouteException;
 import org.zalando.riptide.Route;
 
 public final class RetryRoute {
@@ -9,9 +12,16 @@ public final class RetryRoute {
     }
 
     public static Route retry() {
-        return (response, reader) -> {
+        return Impl.RETRY;
+    }
+
+    private enum Impl implements Route {
+        RETRY;
+
+        @Override
+        public void execute(final ClientHttpResponse response, final MessageReader reader) throws Exception {
             throw new RetryException(response);
-        };
+        }
     }
 
 }
