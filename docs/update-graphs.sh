c8774264eb466c278bd2ce94866ec9c7c9caff9a
Willi Schönborn <willi.schoenborn@zalando.de>
2018-07-06 02:01:00 +0200

    Added hint for my future self

--- docs/update-graphs.sh
+++ docs/update-graphs.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -ex
 
+# colors: https://yuml.me/69f3a9ba.svg
 http --follow --check-status "http://yuml.me/diagram/scruffy/class/$(paste -s -d',' docs/graph.yuml.txt)" > $(dirname $0)/graph.png
