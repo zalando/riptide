6e08c52600de9466375b8ff4c30a1faaa5e59255
Willi Schönborn <willi.schoenborn@zalando.de>
2018-07-17 01:09:27 +0200

    Failsafe plugin now fails ot compile if used incorrectly
    Fixes #442

--- riptide-spring-boot-starter/src/main/java/org/zalando/riptide/spring/FailsafePluginFactory.java
+++ riptide-spring-boot-starter/src/main/java/org/zalando/riptide/spring/FailsafePluginFactory.java
@@ -2,6 +2,7 @@ package org.zalando.riptide.spring;
 
 import net.jodah.failsafe.CircuitBreaker;
 import net.jodah.failsafe.RetryPolicy;
+import org.zalando.riptide.Plugin;
 import org.zalando.riptide.failsafe.CircuitBreakerListener;
 import org.zalando.riptide.failsafe.FailsafePlugin;
 import org.zalando.riptide.failsafe.RetryAfterDelayFunction;
@@ -24,7 +25,7 @@ final class FailsafePluginFactory {
 
     }
 
-    public static FailsafePlugin createFailsafePlugin(final ScheduledExecutorService scheduler,
+    public static Plugin createFailsafePlugin(final ScheduledExecutorService scheduler,
             @Nullable final RetryPolicy retryPolicy, @Nullable final CircuitBreaker circuitBreaker,
             final RetryListener listener) {
         return new FailsafePlugin(scheduler)
