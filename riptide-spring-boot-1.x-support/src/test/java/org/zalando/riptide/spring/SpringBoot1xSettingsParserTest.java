7a9c79520769941637ff5a2f47c1e19c47681ce0
Lukas Niemeier <lukas.niemeier@zalando.de>
2018-10-12 10:24:20 +0200

    Fix placeholder resolving in binding

--- riptide-spring-boot-1.x-support/src/test/java/org/zalando/riptide/spring/SpringBoot1xSettingsParserTest.java
+++ riptide-spring-boot-1.x-support/src/test/java/org/zalando/riptide/spring/SpringBoot1xSettingsParserTest.java
@@ -5,6 +5,8 @@ import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.mock.env.MockEnvironment;
 import org.springframework.validation.BindException;
 
+import java.net.URI;
+
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -19,13 +21,15 @@ public final class SpringBoot1xSettingsParserTest {
     @Test
     public void shouldParse() {
         final ConfigurableEnvironment environment = new MockEnvironment()
-                .withProperty("riptide.clients.example.base-url", "https://example.com");
+                .withProperty("riptide.clients.example.base-url", "https://example.com")
+                .withProperty("riptide.oauth.access-token-url", "${RIPTIDE_TEST_ENV_VAR:https://example.com/oauth}");
         final SettingsParser unit = new SpringBoot1xSettingsParser();
 
         final RiptideProperties settings = unit.parse(environment);
 
         assertThat(settings.getClients().values(), hasSize(1));
         assertThat(settings.getClients().get("example").getBaseUrl(), is("https://example.com"));
+        assertThat(settings.getOauth().getAccessTokenUrl(), is(URI.create("https://example.com/oauth")));
     }
 
     @Test(expected = BindException.class)
