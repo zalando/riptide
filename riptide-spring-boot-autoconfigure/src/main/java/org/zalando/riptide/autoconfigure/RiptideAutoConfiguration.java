373dd7f7c930e7c2aac3f64b450c0fb465e22674
Willi Schönborn <w.schoenborn@gmail.com>
2019-10-02 15:25:44 +0200

    Removed hack that is no longer needed

--- riptide-spring-boot-autoconfigure/src/main/java/org/zalando/riptide/autoconfigure/RiptideAutoConfiguration.java
+++ riptide-spring-boot-autoconfigure/src/main/java/org/zalando/riptide/autoconfigure/RiptideAutoConfiguration.java
@@ -1,20 +1,10 @@
 package org.zalando.riptide.autoconfigure;
 
-import com.google.gag.annotation.remark.Hack;
-import io.micrometer.core.instrument.Clock;
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
 import org.apiguardian.api.API;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
-import java.util.List;
 
 import static org.apiguardian.api.API.Status.INTERNAL;
 import static org.apiguardian.api.API.Status.STABLE;
@@ -25,6 +15,8 @@ import static org.apiguardian.api.API.Status.STABLE;
         "org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration",
         "org.zalando.logbook.autoconfigure.LogbookAutoConfiguration",
         "org.zalando.tracer.autoconfigure.TracerAutoConfiguration",
+        "org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration",
+        "org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration",
 })
 @AutoConfigureBefore(name = {
         "org.zalando.actuate.autoconfigure.failsafe.CircuitBreakersEndpointAutoConfiguration"
@@ -35,24 +27,6 @@ public class RiptideAutoConfiguration {
     @Bean
     public static RiptidePostProcessor riptidePostProcessor() {
         return new RiptidePostProcessor(DefaultRiptideRegistrar::new);
-    }
-
-    @Hack("We need to 'rename' the existing meter registry so we can find it by name...")
-    @Configuration
-    @ConditionalOnClass(MeterRegistry.class)
-    @AutoConfigureAfter(name = {
-            "org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration"
-    })
-    static class MetricsAutoConfiguration {
-
-        @Bean
-        @Primary
-        @ConditionalOnBean({MeterRegistry.class, Clock.class})
-        @ConditionalOnMissingBean(name = "meterRegistry")
-        public CompositeMeterRegistry meterRegistry(final Clock clock, final List<MeterRegistry> registries) {
-            return new CompositeMeterRegistry(clock, registries);
-        }
-
     }
 
 }
--- riptide-spring-boot-autoconfigure/src/test/java/org/zalando/riptide/autoconfigure/MetricsTestAutoConfiguration.java
+++ riptide-spring-boot-autoconfigure/src/test/java/org/zalando/riptide/autoconfigure/MetricsTestAutoConfiguration.java
@@ -3,14 +3,10 @@ package org.zalando.riptide.autoconfigure;
 import org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.metrics.export.simple.SimpleMetricsExportAutoConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@AutoConfigureBefore(RiptideAutoConfiguration.class)
-@ConditionalOnClass(CompositeMeterRegistryAutoConfiguration.class)
 @ImportAutoConfiguration({
         MetricsAutoConfiguration.class,
         SimpleMetricsExportAutoConfiguration.class,
