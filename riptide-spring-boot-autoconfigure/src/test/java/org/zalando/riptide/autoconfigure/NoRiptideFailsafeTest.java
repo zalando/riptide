fa6fa1f1352ec27c3d5eb2406d16555dfb7adab4
Roman Romanchuk <roman.romanchuk@zalando.de>
2020-02-09 14:23:01 +0100

    Added test for #841

--- riptide-spring-boot-autoconfigure/src/test/java/org/zalando/riptide/autoconfigure/NoRiptideFailsafeTest.java
+++ riptide-spring-boot-autoconfigure/src/test/java/org/zalando/riptide/autoconfigure/NoRiptideFailsafeTest.java
@@ -9,8 +9,14 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 final class NoRiptideFailsafeTest {
     @Test
-    void shouldReadClassDefinitionWithoutFailsafePlugin() {
+    void shouldReadMicrometerPluginDefinitionWithoutFailsafePlugin() {
         Method[] methods = ReflectionUtils.getUniqueDeclaredMethods(MicrometerPluginFactory.class);
+        assertNotNull(methods);
+    }
+
+    @Test
+    void shouldReadDefaultRiptideRegistrarDefinitionWithoutFailsafePlugin() {
+        Method[] methods = ReflectionUtils.getUniqueDeclaredMethods(DefaultRiptideRegistrar.class);
         assertNotNull(methods);
     }
 }
