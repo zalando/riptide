ced75776843cbff79b97a814f2b2c4deb111e66f
Roman Romanchuk <roman.romanchuk@zalando.de>
2021-11-08 20:06:26 +0100

    Added parent for spans around calls. Moved headers propagation

--- riptide-opentelemetry/src/main/java/org/zalando/riptide/opentelemetry/OpenTelemetryPlugin.java
+++ riptide-opentelemetry/src/main/java/org/zalando/riptide/opentelemetry/OpenTelemetryPlugin.java
@@ -19,7 +19,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
 public class OpenTelemetryPlugin implements Plugin {
 
@@ -28,8 +27,6 @@ public class OpenTelemetryPlugin implements Plugin {
     private final Tracer tracer;
     private final TextMapPropagator propagator;
 
-    private final Attribute<Span> internalSpan = Attribute.generate();
-
     public OpenTelemetryPlugin(final Tracer tracer) {
         this.tracer = tracer;
         this.propagator = GlobalOpenTelemetry.getPropagators().getTextMapPropagator();
@@ -37,46 +34,28 @@ public class OpenTelemetryPlugin implements Plugin {
 
     @Override
     public RequestExecution aroundAsync(@Nonnull final RequestExecution execution) {
-        return arguments -> trace(execution, arguments);
-    }
-
-    @Override
-    public RequestExecution aroundNetwork(@Nonnull final RequestExecution execution) {
-        return arguments -> inject(execution, arguments);
+        final Context context = Context.current();
+        return arguments -> trace(context, execution, arguments);
     }
 
     private CompletableFuture<ClientHttpResponse> trace(
+            final Context context,
             final RequestExecution execution,
             final RequestArguments arguments) throws IOException {
 
         final String operationName = arguments.getAttribute(OPERATION_NAME)
                                               .orElse(arguments.getMethod().name());
-
         final Span span = tracer.spanBuilder(operationName)
                                 .setSpanKind(SpanKind.CLIENT)
+                                .setParent(context)
                                 .startSpan();
 
-        return execution.execute(arguments.withAttribute(internalSpan, span))
-                        .whenComplete((response, throwable) -> span.end());
-    }
-
-    private CompletableFuture<ClientHttpResponse> inject(
-            final RequestExecution execution,
-            final RequestArguments arguments) throws IOException {
-
-        @Nullable
-        final Span span = arguments.getAttribute(internalSpan)
-                                   .orElse(null);
-
-        if (span == null) {
-            return execution.execute(arguments);
-        }
-
+        final Map<String, String> headers = new HashMap<>();
         try (final Scope ignored = span.makeCurrent()) {
-            span.addEvent("Network call");
-            Map<String, String> headers = new HashMap<>();
             this.propagator.inject(Context.current(), headers, Map::put);
-            return execution.execute(arguments.withHeaders(Multimaps.forMap(headers).asMap()));
         }
+
+        return execution.execute(arguments.withHeaders(Multimaps.forMap(headers).asMap()))
+                        .whenComplete((r, throwable) -> span.end());
     }
 }
