91ac7631f9dc4586345f51fdb9df494bb2c5f382
Olga Semernitskaia <semernitskaya@gmail.com>
2023-03-19 15:01:52 +0200

    mock server - OkHttp test changes

--- riptide-failsafe/src/test/java/org/zalando/riptide/failsafe/HttpMockTestOkHttp.java
+++ riptide-failsafe/src/test/java/org/zalando/riptide/failsafe/HttpMockTestOkHttp.java
@@ -2,11 +2,14 @@ package org.zalando.riptide.failsafe;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.restdriver.clientdriver.ClientDriver;
-import com.github.restdriver.clientdriver.ClientDriverFactory;
 import dev.failsafe.CircuitBreaker;
 import dev.failsafe.RetryPolicy;
+import io.micrometer.core.instrument.util.IOUtils;
 import lombok.SneakyThrows;
+import okhttp3.mockwebserver.Dispatcher;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -28,9 +31,6 @@ import java.time.Duration;
 import java.util.concurrent.CompletionException;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import static com.github.restdriver.clientdriver.RestClientDriver.giveEmptyResponse;
-import static com.github.restdriver.clientdriver.RestClientDriver.giveResponseAsBytes;
-import static com.github.restdriver.clientdriver.RestClientDriver.onRequestTo;
 import static com.google.common.io.Resources.getResource;
 import static java.util.concurrent.Executors.newFixedThreadPool;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
@@ -38,6 +38,7 @@ import static java.util.stream.IntStream.range;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
 import static org.springframework.http.HttpStatus.SERVICE_UNAVAILABLE;
@@ -57,7 +58,7 @@ import static org.zalando.riptide.faults.TransientFaults.transientSocketFaults;
 
 final class HttpMockTestOkHttp {
 
-    private final ClientDriver driver = new ClientDriverFactory().createClientDriver();
+    MockWebServer server = new MockWebServer();
 
     private final CloseableHttpClient client = HttpClientBuilder.create()
             .setDefaultRequestConfig(RequestConfig.custom()
@@ -70,7 +71,7 @@ final class HttpMockTestOkHttp {
     private final Http unit = Http.builder()
             .executor(newFixedThreadPool(2)) // to allow for nested calls
             .requestFactory(new ApacheClientHttpRequestFactory(client))
-            .baseUrl(driver.getBaseUrl())
+            .baseUrl( "http://localhost:" + server.getPort() )
             .converter(createJsonConverter())
             .plugin(new Plugin() {
                 @Override
@@ -112,37 +113,46 @@ final class HttpMockTestOkHttp {
 
     @AfterEach
     void tearDown() throws IOException {
-        driver.verify();
+        //TODO: where server started?
         client.close();
+        server.shutdown();
     }
 
     @SneakyThrows
     @Test
     void shouldRetrySuccessfully() {
-        driver.addExpectation(onRequestTo("/foo"), giveEmptyResponse().after(800, MILLISECONDS));
-        driver.addExpectation(onRequestTo("/foo"), giveResponseAsBytes(getResource("contributors.json").openStream(), "application/json"));
+        //throttleBody to emulate timeout, doesn't work without body
+        server.enqueue(new MockResponse().setResponseCode(200).setBody("Hello").throttleBody(4, 800, MILLISECONDS) );
+        server.enqueue(new MockResponse().setResponseCode(200)
+                .setBody(IOUtils.toString(getResource("contributors.json").openStream()))
+                .setHeader("Content-Type","application/json" ));
 
         unit.get("/foo")
                 .call(pass())
                 .join();
+
+        //can't invoke verify so verifying in every test
+        assertEquals(2, server.getRequestCount());
     }
 
 
     @Test
     void shouldRetryUnsuccessfully() {
         range(0, 5).forEach(i ->
-                driver.addExpectation(onRequestTo("/bar"), giveEmptyResponse().after(800, MILLISECONDS)));
+                server.enqueue(new MockResponse().setResponseCode(200).setBody("Hello").throttleBody(4, 800, MILLISECONDS) ));
 
         final CompletionException exception = assertThrows(CompletionException.class,
                 unit.get("/bar").call(pass())::join);
 
         assertThat(exception.getCause(), is(instanceOf(SocketTimeoutException.class)));
+        assertEquals(5, server.getRequestCount());
     }
 
     @Test
     void shouldRetryExplicitly() {
-        driver.addExpectation(onRequestTo("/baz"), giveEmptyResponse().withStatus(503).withHeader("X-RateLimit-Reset", "1523486068"));
-        driver.addExpectation(onRequestTo("/baz"), giveEmptyResponse());
+
+        server.enqueue(new MockResponse().setResponseCode(503).setHeader("X-RateLimit-Reset", "1523486068"));
+        server.enqueue(new MockResponse().setResponseCode(204));
 
         unit.get("/baz")
                 .dispatch(series(),
@@ -150,16 +160,37 @@ final class HttpMockTestOkHttp {
                         anySeries().dispatch(status(),
                                 on(SERVICE_UNAVAILABLE).call(retry())))
                 .join();
+
+        assertEquals(2, server.getRequestCount());
     }
 
+    @SneakyThrows
     @Test
     void shouldAllowNestedCalls() {
-        driver.addExpectation(onRequestTo("/foo"), giveEmptyResponse());
-        driver.addExpectation(onRequestTo("/bar"), giveEmptyResponse());
+        final Dispatcher dispatcher = new Dispatcher() {
+
+            @Override
+            public MockResponse dispatch (RecordedRequest request) throws InterruptedException {
+
+                //not needed here, just to demonstrate dispatch logic
+                switch (request.getPath()) {
+                    case "/foo":
+                        return new MockResponse().setResponseCode(204);
+                    case "/bar":
+                        return new MockResponse().setResponseCode(204);
+                }
+                return new MockResponse().setResponseCode(404);
+            }
+        };
+        server.setDispatcher(dispatcher);
 
         assertTimeout(Duration.ofSeconds(1),
                 unit.get("/foo")
                         .call(call(() -> unit.get("/bar").call(pass()).join()))::join);
+
+        assertEquals(2, server.getRequestCount());
+        assertEquals("/foo", server.takeRequest().getPath());
+        assertEquals("/bar", server.takeRequest().getPath());
     }
 
 
