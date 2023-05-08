6905d61ed51c59db842611a98ebdb574e956ff30
Olga Semernitskaia <semernitskaya@gmail.com>
2023-05-08 17:49:20 +0300

    mock server - OkHttp test changes for riptide-chaos

--- riptide-chaos/src/test/java/org/zalando/riptide/chaos/ChaosPluginTest.java
+++ riptide-chaos/src/test/java/org/zalando/riptide/chaos/ChaosPluginTest.java
@@ -1,7 +1,7 @@
 package org.zalando.riptide.chaos;
 
-import com.github.restdriver.clientdriver.ClientDriver;
-import com.github.restdriver.clientdriver.ClientDriverFactory;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -23,8 +23,6 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 import java.util.concurrent.Executors;
 
-import static com.github.restdriver.clientdriver.RestClientDriver.giveEmptyResponse;
-import static com.github.restdriver.clientdriver.RestClientDriver.onRequestTo;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.anEmptyMap;
@@ -42,10 +40,13 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.SERVICE_UNAVAILABLE;
 import static org.zalando.riptide.PassRoute.pass;
 import static org.zalando.riptide.chaos.FailureInjection.composite;
+import static org.zalando.riptide.chaos.MockWebServerUtil.emptyMockResponse;
+import static org.zalando.riptide.chaos.MockWebServerUtil.getBaseUrl;
+import static org.zalando.riptide.chaos.MockWebServerUtil.verify;
 
 final class ChaosPluginTest {
 
-    private final ClientDriver driver = new ClientDriverFactory().createClientDriver();
+    private final MockWebServer server = new MockWebServer();
 
     private final Probability latencyProbability = mock(Probability.class);
     private final Probability exceptionProbability = mock(Probability.class);
@@ -60,7 +61,7 @@ final class ChaosPluginTest {
     private final Http unit = Http.builder()
             .executor(Executors.newSingleThreadExecutor())
             .requestFactory(new HttpComponentsClientHttpRequestFactory(client))
-            .baseUrl(driver.getBaseUrl())
+            .baseUrl(getBaseUrl(server))
             .plugin(new ChaosPlugin(composite(
                     new LatencyInjection(latencyProbability, Clock.systemUTC(), Duration.ofSeconds(1)),
                     new ExceptionInjection(exceptionProbability, Arrays.asList(
@@ -81,18 +82,20 @@ final class ChaosPluginTest {
 
     @Test
     void shouldNotInjectError() {
-        driver.addExpectation(onRequestTo("/foo"), giveEmptyResponse());
+        server.enqueue(emptyMockResponse());
 
         unit.get("/foo")
                 .call(pass())
                 .join();
+
+        verify(server, 1, "/foo");
     }
 
     @Test
     void shouldInjectLatency() {
         when(latencyProbability.test()).thenReturn(true);
 
-        driver.addExpectation(onRequestTo("/foo"), giveEmptyResponse());
+        server.enqueue(emptyMockResponse());
 
         final Clock clock = Clock.systemUTC();
         final Instant start = clock.instant();
@@ -104,13 +107,15 @@ final class ChaosPluginTest {
         final Instant end = clock.instant();
 
         assertThat(Duration.between(start, end), is(greaterThanOrEqualTo(Duration.ofSeconds(1))));
+        verify(server, 1, "/foo");
     }
 
     @Test
     void shouldNotInjectLatencyIfDelayedAlready() {
         when(latencyProbability.test()).thenReturn(true);
 
-        driver.addExpectation(onRequestTo("/foo"), giveEmptyResponse().after(1, SECONDS));
+        server.enqueue(emptyMockResponse().setHeadersDelay(1, SECONDS));
+
 
         final Clock clock = Clock.systemUTC();
         final Instant start = clock.instant();
@@ -122,13 +127,14 @@ final class ChaosPluginTest {
         final Instant end = clock.instant();
 
         assertThat(Duration.between(start, end), is(lessThan(Duration.ofSeconds(2))));
+        verify(server, 1, "/foo");
     }
 
     @Test
     void shouldInjectErrorResponse() throws IOException {
         when(errorResponseProbability.test()).thenReturn(true);
 
-        driver.addExpectation(onRequestTo("/foo"), giveEmptyResponse());
+        server.enqueue(emptyMockResponse());
 
         final ClientHttpResponse response = unit.get("/foo")
                 .call(pass())
@@ -141,26 +147,28 @@ final class ChaosPluginTest {
         assertThat(response.getRawStatusCode(), is(oneOf(500, 503)));
         assertThat(response.getStatusText(), is(oneOf("Internal Server Error", "Service Unavailable")));
         assertThat(response.getHeaders(), is(anEmptyMap())); // TODO can we do better?
+        verify(server, 1, "/foo");
     }
 
     @Test
     void shouldNotInjectErrorResponseIfFailedAlready() throws IOException {
         when(errorResponseProbability.test()).thenReturn(true);
 
-        driver.addExpectation(onRequestTo("/foo"), giveEmptyResponse().withStatus(400));
+        server.enqueue(new MockResponse().setResponseCode(400));
 
         final ClientHttpResponse response = unit.get("/foo")
                 .call(pass())
                 .join();
 
         assertThat(response.getStatusCode(), is(BAD_REQUEST));
+        verify(server, 1, "/foo");
     }
 
     @Test
     void shouldInjectException() {
         when(exceptionProbability.test()).thenReturn(true);
 
-        driver.addExpectation(onRequestTo("/foo"), giveEmptyResponse());
+        server.enqueue(emptyMockResponse());
 
         final CompletableFuture<ClientHttpResponse> future = unit.get("/foo")
                 .call(pass());
@@ -170,13 +178,14 @@ final class ChaosPluginTest {
         assertThat(exception.getCause(), anyOf(
                 instanceOf(ConnectException.class),
                 instanceOf(NoRouteToHostException.class)));
+        verify(server, 1, "/foo");
     }
 
     @Test
     void shouldNotInjectExceptionIfThrownAlready() {
         when(exceptionProbability.test()).thenReturn(true);
 
-        driver.addExpectation(onRequestTo("/foo"), giveEmptyResponse().after(2, SECONDS));
+        server.enqueue(emptyMockResponse().setHeadersDelay(2, SECONDS));
 
         final CompletableFuture<ClientHttpResponse> future = unit.get("/foo")
                 .call(pass());
@@ -185,6 +194,7 @@ final class ChaosPluginTest {
         final Throwable cause = exception.getCause();
 
         assertThat(cause, is(instanceOf(SocketTimeoutException.class)));
+        verify(server, 1, "/foo");
     }
 
     @Test
@@ -192,7 +202,7 @@ final class ChaosPluginTest {
         when(latencyProbability.test()).thenReturn(true);
         when(errorResponseProbability.test()).thenReturn(true);
 
-        driver.addExpectation(onRequestTo("/foo"), giveEmptyResponse());
+        server.enqueue(emptyMockResponse());
 
         final Clock clock = Clock.systemUTC();
         final Instant start = clock.instant();
@@ -205,6 +215,7 @@ final class ChaosPluginTest {
 
         assertThat(Duration.between(start, end), is(greaterThanOrEqualTo(Duration.ofSeconds(1))));
         assertThat(response.getRawStatusCode(), is(oneOf(500, 503)));
+        verify(server, 1, "/foo");
     }
 
 }
