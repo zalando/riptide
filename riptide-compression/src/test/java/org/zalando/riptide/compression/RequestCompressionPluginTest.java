72472f6039f6e676669d479d0dc2e0b923769fd3
Olga Semernitskaia <semernitskaya@gmail.com>
2023-05-08 19:18:21 +0300

    mock server - OkHttp test changes for riptide-compression, riptide-soap

--- riptide-compression/src/test/java/org/zalando/riptide/compression/RequestCompressionPluginTest.java
+++ riptide-compression/src/test/java/org/zalando/riptide/compression/RequestCompressionPluginTest.java
@@ -1,6 +1,8 @@
 package org.zalando.riptide.compression;
 
-import com.github.restdriver.clientdriver.ClientDriver;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import okio.Buffer;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -17,41 +19,42 @@ import org.zalando.riptide.Plugin;
 import org.zalando.riptide.httpclient.ApacheClientHttpRequestFactory;
 import org.zalando.riptide.httpclient.ApacheClientHttpRequestFactory.Mode;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
+import java.util.zip.GZIPInputStream;
 
-import static com.github.restdriver.clientdriver.ClientDriverRequest.Method.POST;
-import static com.github.restdriver.clientdriver.RestClientDriver.giveResponse;
-import static com.github.restdriver.clientdriver.RestClientDriver.onRequestTo;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Arrays.asList;
 import static java.util.concurrent.Executors.newSingleThreadExecutor;
-import static org.hamcrest.Matchers.emptyString;
-import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.springframework.http.HttpHeaders.CONTENT_ENCODING;
 import static org.zalando.riptide.PassRoute.pass;
+import static org.zalando.riptide.compression.MockWebServerUtil.emptyTextPlainMockResponse;
+import static org.zalando.riptide.compression.MockWebServerUtil.getBaseUrl;
+import static org.zalando.riptide.compression.MockWebServerUtil.getRecorderRequest;
 
+//TODO: notify that it is impossible to test with Jetty GzipHandler after switching to okhttp MockWebServer
 class RequestCompressionPluginTest {
 
-    private final ClientDriver driver = GzipClientDriver.create();
+    private final MockWebServer server = new MockWebServer();
+
     private final ExecutorService executor = newSingleThreadExecutor();
 
     @AfterEach
-    void tearDown() throws Exception {
+    void tearDown() {
         executor.shutdown();
-        executor.awaitTermination(10, TimeUnit.SECONDS);
-        driver.verify();
     }
 
     @ParameterizedTest
     @ArgumentsSource(RequestFactorySource.class)
     void shouldCompressRequestBody(final ClientHttpRequestFactory factory) {
-        driver.addExpectation(onRequestTo("/")
-                        .withMethod(POST)
-                        .withHeader("X-Content-Encoding", "gzip") // written by Jetty's GzipHandler
-                        .withBody(equalTo("{}"), "application/json"),
-                giveResponse("", "text/plain"));
+        server.enqueue(emptyTextPlainMockResponse());
 
         final Http http = buildHttp(factory, new RequestCompressionPlugin());
         http.post("/")
@@ -59,34 +62,41 @@ class RequestCompressionPluginTest {
                 .body(new HashMap<>())
                 .call(pass())
                 .join();
+
+        RecordedRequest recordedRequest = getRecorderRequest(server);
+        assertNotNull(recordedRequest);
+        assertEquals("/", recordedRequest.getPath());
+        assertEquals("POST", recordedRequest.getMethod());
+        assertEquals("gzip", recordedRequest.getHeaders().get("Content-Encoding"));
+        assertEquals("application/json", recordedRequest.getHeaders().get("Content-Type"));
+        assertEquals("{}", decompressIfNeeded(recordedRequest.getBody()));
+
     }
 
     @ParameterizedTest
     @ArgumentsSource(RequestFactorySource.class)
     void shouldNotCompressEmptyRequestBody(final ClientHttpRequestFactory factory) {
-        driver.addExpectation(onRequestTo("/")
-                        .withMethod(POST)
-                        .withBody(emptyString(), "application/json")
-                        .withoutHeader("Content-Encoding")
-                        .withoutHeader("X-Content-Encoding"),
-                giveResponse("", "text/plain"));
+        server.enqueue(emptyTextPlainMockResponse());
 
         final Http http = buildHttp(factory, new RequestCompressionPlugin());
         http.post("/")
                 .contentType(MediaType.APPLICATION_JSON)
                 .call(pass())
                 .join();
+
+        RecordedRequest recordedRequest = getRecorderRequest(server);
+        assertNotNull(recordedRequest);
+        assertEquals("/", recordedRequest.getPath());
+        assertEquals("POST", recordedRequest.getMethod());
+        assertEquals("application/json", recordedRequest.getHeaders().get("Content-Type"));
+        assertNull(recordedRequest.getHeaders().get("Content-Encoding"));
+        assertEquals("", recordedRequest.getBody().readString(UTF_8));
     }
 
     @ParameterizedTest
     @ArgumentsSource(RequestFactorySource.class)
     void shouldCompressWithGivenAlgorithm(final ClientHttpRequestFactory factory) {
-        driver.addExpectation(onRequestTo("/")
-                        .withMethod(POST)
-                        .withHeader("Content-Encoding", "identity") // not handled by Jetty
-                        .withoutHeader("X-Content-Encoding")
-                        .withBody(equalTo("{}"), "application/json"),
-                giveResponse("", "text/plain"));
+        server.enqueue(emptyTextPlainMockResponse());
 
         final Http http = buildHttp(factory, new RequestCompressionPlugin(Compression.of("identity", it -> it)));
         http.post("/")
@@ -94,17 +104,21 @@ class RequestCompressionPluginTest {
                 .body(new HashMap<>())
                 .call(pass())
                 .join();
+
+
+        RecordedRequest recordedRequest = getRecorderRequest(server);
+        assertNotNull(recordedRequest);
+        assertEquals("/", recordedRequest.getPath());
+        assertEquals("POST", recordedRequest.getMethod());
+        assertEquals("application/json", recordedRequest.getHeaders().get("Content-Type"));
+        assertEquals("identity", recordedRequest.getHeaders().get("Content-Encoding"));
+        assertEquals("{}", decompressIfNeeded(recordedRequest.getBody()));
     }
 
     @ParameterizedTest
     @ArgumentsSource(RequestFactorySource.class)
     void shouldBackOffIfAlreadyEncoded(final ClientHttpRequestFactory factory) {
-        driver.addExpectation(onRequestTo("/")
-                        .withMethod(POST)
-                        .withHeader("Content-Encoding", "custom") // not handled by Jetty
-                        .withoutHeader("X-Content-Encoding")
-                        .withBody(equalTo("{}"), "application/json"),
-                giveResponse("", "text/plain"));
+        server.enqueue(emptyTextPlainMockResponse());
 
         final Http http = buildHttp(factory, new RequestCompressionPlugin());
         http.post("/")
@@ -113,13 +127,46 @@ class RequestCompressionPluginTest {
                 .body(new HashMap<>())
                 .call(pass())
                 .join();
+
+        RecordedRequest recordedRequest = getRecorderRequest(server);
+        assertNotNull(recordedRequest);
+        assertEquals("/", recordedRequest.getPath());
+        assertEquals("POST", recordedRequest.getMethod());
+        assertEquals("application/json", recordedRequest.getHeaders().get("Content-Type"));
+        assertEquals("custom", recordedRequest.getHeaders().get("Content-Encoding"));
+        assertEquals("{}", decompressIfNeeded(recordedRequest.getBody()));
+    }
+
+    private static String decompressIfNeeded(Buffer body) {
+        if (isCompressed(body)) {
+            try (GZIPInputStream gis = new GZIPInputStream(body.inputStream())) {
+                final StringBuilder outStr = new StringBuilder();
+                final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(gis, UTF_8));
+
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    outStr.append(line);
+                }
+                return outStr.toString();
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return body.readString(UTF_8);
+        }
+    }
+
+    public static boolean isCompressed(Buffer body) {
+        return (body.getByte(0) == (byte) (GZIPInputStream.GZIP_MAGIC))
+                && (body.getByte(1) == (byte) (GZIPInputStream.GZIP_MAGIC >> 8));
     }
 
     private Http buildHttp(final ClientHttpRequestFactory factory, final Plugin... plugins) {
         return Http.builder()
                 .executor(executor)
                 .requestFactory(factory)
-                .baseUrl(driver.getBaseUrl())
+                .baseUrl(getBaseUrl(server))
                 .plugins(asList(plugins))
                 .build();
     }
