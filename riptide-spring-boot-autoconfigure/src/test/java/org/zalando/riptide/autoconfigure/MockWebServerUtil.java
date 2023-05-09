40ec20600dfd78c10d10f96b5bf8011dd8b987c0
Olga Semernitskaia <semernitskaya@gmail.com>
2023-05-09 12:59:04 +0300

    mock server - OkHttp test changes for riptide Spring Boot

---
+++ riptide-spring-boot-autoconfigure/src/test/java/org/zalando/riptide/autoconfigure/MockWebServerUtil.java
@@ -0,0 +1,99 @@
+package org.zalando.riptide.autoconfigure;
+
+import okhttp3.Headers;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.springframework.http.HttpStatus;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+
+import static com.google.common.io.Resources.getResource;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.stream.IntStream.range;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.HttpStatus.OK;
+
+public class MockWebServerUtil {
+
+    public static String getBaseUrl(MockWebServer server) {
+        return String.format("http://%s:%s", server.getHostName(), server.getPort());
+    }
+
+    public static MockResponse emptyMockResponse() {
+        return new MockResponse().setResponseCode(NO_CONTENT.value());
+    }
+
+    public static MockResponse emptyTextPlainMockResponse() {
+        return new MockResponse()
+                .setResponseCode(HttpStatus.OK.value())
+                .setBody("")
+                .setHeader("Content-Type", "text/plain");
+    }
+
+    public static MockResponse jsonMockResponseFromResource(String resourceName) throws IOException {
+        return new MockResponse().setResponseCode(OK.value())
+                .setBody(readResourceAsString(resourceName))
+                .setHeader("Content-Type","application/json");
+    }
+
+    public static MockResponse jsonMockResponse(String body) {
+        return new MockResponse().setResponseCode(OK.value())
+                .setBody(body)
+                .setHeader("Content-Type","application/json");
+    }
+    public static void verify(MockWebServer server,
+                              int expectedRequestsCount,
+                              String expectedPath,
+                              Consumer<Headers> headersVerifier) {
+        assertEquals(expectedRequestsCount, server.getRequestCount());
+        range(0, expectedRequestsCount).forEach(i -> {
+            try {
+                RecordedRequest recordedRequest = server.takeRequest(5, TimeUnit.SECONDS);
+                assertNotNull(recordedRequest);
+                assertEquals(expectedPath, recordedRequest.getPath());
+                headersVerifier.accept(recordedRequest.getHeaders());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    public static RecordedRequest getRecorderRequest(MockWebServer server) {
+        try {
+            return server.takeRequest(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void verify(MockWebServer server,
+                              int expectedRequestsCount,
+                              String expectedPath) {
+        verify(server, expectedRequestsCount, expectedPath, headers -> {
+        });
+    }
+
+    public static void verify(MockWebServer server, String... expectedPaths) {
+
+        assertEquals(expectedPaths.length, server.getRequestCount());
+        for (String expectedPath : expectedPaths) {
+            try {
+                RecordedRequest recordedRequest = server.takeRequest(5, TimeUnit.SECONDS);
+                assertEquals(expectedPath, recordedRequest.getPath());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static String readResourceAsString(String resourceName) throws IOException {
+        try (var inputStream = getResource(resourceName).openStream()) {
+            return new String(inputStream.readAllBytes(), UTF_8);
+        }
+    }
+}
