d3a627ee988ca28fb2dcafa2cbf7aaa9942e7ec9
Roman Romanchuk <roman.romanchuk@zalando.de>
2023-06-05 13:38:14 +0200

    Use JDK8 functionality for list creation

--- riptide-opentracing/src/main/java/org/zalando/riptide/opentracing/span/ServiceLoaderSpanDecorator.java
+++ riptide-opentracing/src/main/java/org/zalando/riptide/opentracing/span/ServiceLoaderSpanDecorator.java
@@ -4,6 +4,7 @@ import org.apiguardian.api.API;
 
 import java.util.List;
 import java.util.ServiceLoader;
+import java.util.stream.Collectors;
 
 import static java.util.ServiceLoader.load;
 import static java.util.stream.StreamSupport.stream;
@@ -21,7 +22,7 @@ public final class ServiceLoaderSpanDecorator extends ForwardingSpanDecorator {
     }
 
     private static synchronized List<SpanDecorator> loadDecorators() {
-        return stream(load(SpanDecorator.class).spliterator(), false).toList();
+        return stream(load(SpanDecorator.class).spliterator(), false).collect(Collectors.toList());
     }
 
 }
