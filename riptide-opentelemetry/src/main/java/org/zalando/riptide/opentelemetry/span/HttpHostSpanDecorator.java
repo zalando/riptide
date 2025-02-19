cc8b7173f870e9dcb769a58abde07eda8bdc46df
Christoph Berg <christoph.berg@zalando.de>
2025-02-19 15:58:11 +0100

    Bumped opentelemetry-semconv to the latest version  - Added opentelemetry-semconv-incubating to have access to constants    for used attributes  - Switched used attributes to be imported from classes replacing    SemanticAttributes

--- riptide-opentelemetry/src/main/java/org/zalando/riptide/opentelemetry/span/HttpHostSpanDecorator.java
+++ riptide-opentelemetry/src/main/java/org/zalando/riptide/opentelemetry/span/HttpHostSpanDecorator.java
@@ -1,13 +1,13 @@
 package org.zalando.riptide.opentelemetry.span;
 
 import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.semconv.SemanticAttributes;
+import io.opentelemetry.semconv.incubating.HttpIncubatingAttributes;
 import org.zalando.riptide.RequestArguments;
 
 public class HttpHostSpanDecorator implements SpanDecorator {
 
     @Override
     public void onRequest(final Span span, final RequestArguments arguments) {
-        span.setAttribute(SemanticAttributes.HTTP_HOST, arguments.getRequestUri().getHost());
+        span.setAttribute(HttpIncubatingAttributes.HTTP_HOST, arguments.getRequestUri().getHost());
     }
 }
