45985505f884cc24ba540c56a218b2153cf17ec7
Roman Romanchuk <roman.romanchuk+opensource@zalando.de>
2026-01-19 20:54:56 +0100

    Added missing javadoc

--- riptide-problem/src/main/java/org/zalando/riptide/problem/ProblemResponseException.java
+++ riptide-problem/src/main/java/org/zalando/riptide/problem/ProblemResponseException.java
@@ -3,10 +3,21 @@ package org.zalando.riptide.problem;
 import lombok.Getter;
 import org.springframework.http.ProblemDetail;
 
+/**
+ * Exception thrown when a response contains a {@link ProblemDetail}.
+ * <p>
+ * This exception wraps the {@link ProblemDetail} object, allowing clients to access
+ * the problem details returned from a remote service or API.
+ */
 @Getter
 public class ProblemResponseException extends RuntimeException {
     private final ProblemDetail problem;
 
+    /**
+     * Constructs a new {@code ProblemResponseException} with the specified {@link ProblemDetail}.
+     *
+     * @param problem the problem detail associated with the response
+     */
     public ProblemResponseException(ProblemDetail problem) {
         super(problem.getTitle());
         this.problem = problem;
